8673de00ebea697db337d0040cef0f89f4d891cb
Arnav <asaklani2006@gmail.com>
2026-07-18 00:21:11 +0530

    added proper routing , and base .html

--- SmartRoutePlanner/routing/urls.py
+++ SmartRoutePlanner/routing/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.hello),
+    path('',views.mainreq),
+    path('calculate-route/',views.calculate_route, name="calculate_route")
 ]
--- SmartRoutePlanner/routing/views.py
+++ SmartRoutePlanner/routing/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def hello(request):
-    return render(request,"routing/home.html")
+def mainreq(request):
+    if request.method == "GET":
+        return render(request,"routing/home.html")
+
+def calculate_route(request):
+    if request.method == "POST":
+        destination = request.POST["destination"]
+        source = request.POST["source"]
+        context = {
+        "destination": destination,
+        "source": source
+        }
+        
+        return render(request,"routing/results.html",context) 
