1a2c55f2e1443e556f16d213cfe894f2dc1be55d
Arnav <asaklani2006@gmail.com>
2026-07-17 21:02:29 +0530

    needed one app

--- SmartRoutePlanner/SmartRoutePlanner/settings.py
+++ SmartRoutePlanner/SmartRoutePlanner/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'routes',
-    'core'
-    'navigation',
     'routing',
 ]
 
