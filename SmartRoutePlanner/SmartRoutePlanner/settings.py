835f9adc9dbb058faa9ee7455788c6331970711f
Arnav <asaklani2006@gmail.com>
2026-07-17 20:58:23 +0530

    added apps in the settings

--- SmartRoutePlanner/SmartRoutePlanner/settings.py
+++ SmartRoutePlanner/SmartRoutePlanner/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'routes',
+    'core'
+    'navigation',
+    'routing',
 ]
 
 MIDDLEWARE = [
